18159bde9cb3b2e215ce34b5bdb9efeaaeefe4d1
jazzysnake <dotya77@gmail.com>
2023-10-23 16:31:04 +0200

    containerized delay prediction

---
+++ backend/delay_pred/lda_delay_api.py
@@ -0,0 +1,62 @@
+import os
+import datetime as dt
+
+import pandas as pd
+
+from pycaret.classification import load_model, predict_model
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+model_name = os.environ.get('MODEL_NAME')
+model_path = model_name if model_name is not None else 'lda_delay_api'
+model = load_model(model_path)
+
+
+class DelayPredictionRequest(BaseModel):
+    route: str
+    train_number: str
+    depart_time: dt.datetime
+
+
+class DelayPredictionResponse(BaseModel):
+    label: int
+    score: float
+
+
+@app.post('/predict')
+def predict(data: DelayPredictionRequest):
+    pred_data = transform_request_to_training_data(data)
+    predictions = predict_model(model, data=pred_data)
+
+    return DelayPredictionResponse(
+        label=int(predictions['prediction_label'].iloc[0]),
+        score=float(predictions['prediction_score'].iloc[0]),
+    )
+
+
+def transform_request_to_training_data(
+    req: DelayPredictionRequest,
+) -> pd.DataFrame:
+    line_kind = 'GYSEV'
+    if req.train_number.startswith('55'):
+        line_kind = 'MAV'
+    elif req.train_number.startswith('36H'):
+        line_kind = 'HEV'
+    tn_len = (
+        len(req.train_number) - 3
+        if line_kind == 'HEV'
+        else len(req.train_number) - 2
+    )
+    return pd.DataFrame(
+        [
+            {
+                'route': req.route,
+                'tn_len': tn_len,
+                'line_kind': line_kind,
+                'dow': req.depart_time.weekday(),
+                'month': req.depart_time.month,
+            }
+        ]
+    )

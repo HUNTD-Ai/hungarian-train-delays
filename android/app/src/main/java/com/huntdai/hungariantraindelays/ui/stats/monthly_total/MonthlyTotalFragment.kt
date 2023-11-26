fbf3da13c839f6c67f7f3aa4fd36a3bca352dd05
Kapus Ádám <64330090+adamkapus@users.noreply.github.com>
2023-11-27 00:06:28 +0100

    41 Improve UI on monthly total fragment

--- android/app/src/main/java/com/huntdai/hungariantraindelays/ui/stats/monthly_total/MonthlyTotalFragment.kt
+++ android/app/src/main/java/com/huntdai/hungariantraindelays/ui/stats/monthly_total/MonthlyTotalFragment.kt
@@ -1,30 +1,41 @@
 package com.huntdai.hungariantraindelays.ui.stats.monthly_total
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.huntdai.hungariantraindelays.R
 import com.huntdai.hungariantraindelays.data.network.stats.models.Delay
 import com.huntdai.hungariantraindelays.databinding.FragmentMonthlyMeanBinding
 import com.huntdai.hungariantraindelays.databinding.FragmentMonthlyTotalBinding
 import com.huntdai.hungariantraindelays.ui.stats.monthly_mean.MonthlyMeanUIState
 import com.huntdai.hungariantraindelays.ui.stats.monthly_mean.MonthlyMeanViewModel
+import com.huntdai.hungariantraindelays.utils.getCurrentTextcolor
+import com.huntdai.hungariantraindelays.utils.getMonthNameFromTimestamp
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MonthlyTotalFragment : Fragment() {
     private val viewModel: MonthlyTotalViewModel by viewModels()
-    private lateinit var chart : BarChart
+    private lateinit var chart: BarChart
+    private lateinit var loadProgressBar: ProgressBar
+    private lateinit var errorText: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +43,9 @@ class MonthlyTotalFragment : Fragment() {
     ): View? {
         val binding = FragmentMonthlyTotalBinding.inflate(layoutInflater)
         chart = binding.chart
+        loadProgressBar = binding.loadProgressbar
+        errorText = binding.errorText
+        binding.title.text = "Monthly Total delays"
         setupChart()
         return binding.root
     }
@@ -49,91 +63,89 @@ class MonthlyTotalFragment : Fragment() {
 
     private fun render(uiState: MonthlyTotalUIState) {
         when (uiState) {
-            is MonthlyTotalUIState.Initial -> {}
+            is MonthlyTotalUIState.Initial -> {
+                chart.visibility = View.GONE
+                loadProgressBar.visibility = View.GONE
+                errorText.visibility = View.GONE
+            }
+
             is MonthlyTotalUIState.Loaded -> {
+                chart.visibility = View.VISIBLE
+                loadProgressBar.visibility = View.GONE
+                errorText.visibility = View.GONE
                 populateChart(uiState.delays)
             }
-            is MonthlyTotalUIState.Loading -> {}
-            is MonthlyTotalUIState.Error -> {}
+
+            is MonthlyTotalUIState.Loading -> {
+                chart.visibility = View.GONE
+                loadProgressBar.visibility = View.VISIBLE
+                errorText.visibility = View.GONE
+            }
+
+            is MonthlyTotalUIState.Error -> {
+                chart.visibility = View.GONE
+                loadProgressBar.visibility = View.GONE
+                errorText.visibility = View.VISIBLE
+            }
         }
     }
 
-    private fun populateChart(delays: List<Delay>){
+    private fun populateChart(delays: List<Delay>) {
         val entries = ArrayList<BarEntry>()
-        val title = "Title"
+        val labels = mutableListOf<String>()
+
+        Log.d("DEMO", "TOTALLLIST" + delays.toString())
 
         for (i in delays.indices) {
             val barEntry = delays[i].delay?.let { BarEntry(i.toFloat(), it.toFloat()) }
             if (barEntry != null) {
                 entries.add(barEntry)
+                val monthName = delays[i].timestamp?.let { getMonthNameFromTimestamp(it) }
+                if (monthName != null) {
+                    labels.add(monthName)
+                }
             }
         }
 
-        val barDataSet = BarDataSet(entries, title)
+        val barDataSet = BarDataSet(entries, "Minutes")
+        barDataSet.color = ContextCompat.getColor(requireContext(), R.color.primary);
+        barDataSet.valueTextColor = getCurrentTextcolor(requireContext())
+
+        val description = Description()
+        description.isEnabled = false
+        chart.description = description
 
         val data = BarData(barDataSet)
         chart.data = data
+
+        // Customize X-axis with month names as labels
+        val xAxis = chart.xAxis
+        xAxis.setDrawGridLines(false)
+        xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.granularity = 1f
+        xAxis.isGranularityEnabled = true
+
+
         chart.invalidate()
     }
 
-    private fun setupChart(){
-        chart.setDrawGridBackground(false);
-        chart.setDrawBarShadow(false)
-        chart.setDrawBorders(false)
+    private fun setupChart() {
+        val rightAxis = chart.axisRight
+        rightAxis.setDrawLabels(false)
 
-        val description =  Description()
-        description.isEnabled = false
-        chart.description = description
+        val textColor = getCurrentTextcolor(requireContext())
+
+        val xAxis = chart.xAxis
+        xAxis.textColor = textColor
+
+        val leftAxis = chart.axisLeft
+        leftAxis.textColor = textColor
+
+        chart.description.textColor = textColor
+        chart.legend.textColor = textColor
 
         chart.animateY(1000);
         chart.animateX(1000);
     }
-
-
-    /*barChart.setDrawGridBackground(false);
-    //remove the bar shadow, default false if not set
-    barChart.setDrawBarShadow(false);
-    //remove border of the chart, default false if not set
-    barChart.setDrawBorders(false);
-
-    //remove the description label text located at the lower right corner
-    Description description = new Description();
-    description.setEnabled(false);
-    barChart.setDescription(description);
-
-    //setting animation for y-axis, the bar will pop up from 0 to its value within the time we set
-    barChart.animateY(1000);
-    //setting animation for x-axis, the bar will pop up separately within the time we set
-    barChart.animateX(1000);
-
-    XAxis xAxis = barChart.getXAxis();
-    //change the position of x-axis to the bottom
-    xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-    //set the horizontal distance of the grid line
-    xAxis.setGranularity(1f);
-    //hiding the x-axis line, default true if not set
-    xAxis.setDrawAxisLine(false);
-    //hiding the vertical grid lines, default true if not set
-    xAxis.setDrawGridLines(false);
-
-    YAxis leftAxis = barChart.getAxisLeft();
-    //hiding the left y-axis line, default true if not set
-    leftAxis.setDrawAxisLine(false);
-
-    YAxis rightAxis = barChart.getAxisRight();
-    //hiding the right y-axis line, default true if not set
-    rightAxis.setDrawAxisLine(false);
-
-    Legend legend = barChart.getLegend();
-    //setting the shape of the legend form to line, default square shape
-    legend.setForm(Legend.LegendForm.LINE);
-    //setting the text size of the legend
-    legend.setTextSize(11f);
-    //setting the alignment of legend toward the chart
-    legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-    legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
-    //setting the stacking direction of legend
-    legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-    //setting the location of legend outside the chart, default false if not set
-    legend.setDrawInside(false);*/
 }
